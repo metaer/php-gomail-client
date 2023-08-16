719adec787b0e8ee9d2f270f745e9e6c8a56e7b0
Pavel Popov <pavelpopovw@gmail.com>
2023-08-17 03:24:30 +0700

    Initial commit

---
+++ example.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'vendor/autoload.php';
+
+const USERNAME = 'username'; //Необходимо заменить на реальный логин
+const PASSWORD = 'password'; //Необходимо заменить на реальный пароль
+CONST EMAIL_TO = 'example@example.com'; //Необходимо заменить на email получателя
+
+try{
+    $mail = new PHPMailer(true);
+
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.mailer-demo.ru';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = USERNAME;
+    $mail->Password   = PASSWORD;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
+    $mail->CharSet    = "UTF-8";
+
+    /*
+     * Отправителя тоже можно заменить на своего.
+     * Для этого нужно к своему домену добавить TXT-запись SPF вида
+     * v=spf1 include:spf.mailer-demo.ru
+     */
+    $mail->setFrom('admin@mailer-demo.ru', 'Mailer-demo.ru');
+    $mail->addAddress(EMAIL_TO);
+    $mail->Subject = 'Тестовая тема';
+    $mail->Body    = 'Тестовое тело письма';
+
+    $mail->send();
+    echo 'Письмо успешно отправлено';
+} catch (Exception $e) {
+    echo PHP_EOL, "Письмо не отправлено. Ошибка: {$e->getMessage()}", PHP_EOL;
+}
