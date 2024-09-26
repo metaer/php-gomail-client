048bb4728c5a8038969007547b2ee8e0f64b8d14
Pavel Popov <pavelpopovw@gmail.com>
2024-09-26 19:29:44 +0300

    Константы

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 Как запустить демо:
 
 1. `git clone https://github.com/metaer/php-gomail-client`
-2. `composer install`
-3. В трёх константах в файле `example.php` задать свои значения
-4. `php example.php`
+2. `cd php-gomail-client`
+3. `composer install`
+4. В константах в файле `example.php` задать свои значения
+5. `php example.php`
 
 Как использовать в своём проекте:
 
--- example.php
+++ example.php
@@ -11,18 +11,21 @@ require 'vendor/autoload.php';
 const SMTP_SERVER_USERNAME = 'username'; // Необходимо заменить на реальный логин
 const SMTP_SERVER_PASSWORD = 'password'; // Необходимо заменить на реальный пароль
 CONST EMAIL_TO = 'example@example.com'; // Необходимо заменить на email получателя
+const HOST = 'smtp.example.com'; // Необходимо заменить на хост smtp-сервера
+const PORT = 587; // Необходимо заменить на порт smtp-сервера
+const EMAIL_FROM = 'example@example.com'; // Необходимо заменить на email отправителя
 
 try{
     $mail = new PHPMailer(true);
 
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;
     $mail->isSMTP();
-    $mail->Host       = 'smtp.mailer-demo.ru';
+    $mail->Host       = HOST;
     $mail->SMTPAuth   = true;
     $mail->Username   = SMTP_SERVER_USERNAME;
     $mail->Password   = SMTP_SERVER_PASSWORD;
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port       = 587;
+    $mail->Port       = PORT;
     $mail->CharSet    = "UTF-8";
 
     /*
@@ -30,7 +33,7 @@ try{
      * Для этого нужно к своему домену добавить TXT-запись SPF вида
      * v=spf1 include:spf.mailer-demo.ru
      */
-    $mail->setFrom('admin@mailer-demo.ru', 'Mailer-demo.ru');
+    $mail->setFrom(EMAIL_FROM, ucfirst(explode('@', EMAIL_FROM)[1]));
     $mail->addAddress(EMAIL_TO);
     $mail->Subject = 'Тестовая тема';
     $mail->Body    = 'Тестовое тело письма';
